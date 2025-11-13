d565acb723bfcf99a83d9539731674f1720ef271
Rehan Shaikh <rr.gamer5903@gmail.com>
2025-11-13 10:05:28 +0000

    Add data.py with variable and operator examples

---
+++ my/data.py
@@ -0,0 +1,27 @@
+"""
+
+Name = #videable
+
+50 #integer 
+Rehan #string
+49.83 #floating
+
+------------------------------
+
+== (Double equals: kya barabar hai?) 
+
+> (Kya bada hai?) (Jaise age > 18)
+
+< (Kya chhota hai?) (Jaise age < 18)
+
+>= (Kya bada ya barabar hai?)
+
+<= (Kya chhota ya barabar hai?)
+
+------------------------------
+
+[] = list
+() = tuple
+{} = Dictionary
+
+"""
